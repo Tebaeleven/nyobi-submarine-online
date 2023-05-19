bb8edac45c5861b95a8182f5d2f664de0e2e88af
nano <nano@nanonoMacBook-Air.local>
2023-05-19 17:25:04 +0900

    アイテムの描画

--- app/entry.js
+++ app/entry.js
@@ -45,8 +45,12 @@ function init() {
 init()
 
 function ticker() {
+    //データが受信されていないときは何もしない
+    if (!gameObj.myPlayerObj || !gameObj.playersMap) return;
+
     gameObj.ctxRader.clearRect(0, 0, gameObj.raderCanvasWidth, gameObj.raderCanvasHeight);
     drawRadar(gameObj.ctxRader);
+    drawMap(gameObj);
     drawSubmarine(gameObj.ctxRader);
 }
 
@@ -146,4 +150,118 @@ socket.on('map data', (compressed) => {
 });
 function getRadian(deg) { //角度をラジアンに変換
     return deg * Math.PI / 180
+}
+
+function drawMap(gameObj) {
+    drawObj(gameObj.itemsMap, 255, 165, 0);
+    drawObj(gameObj.airMap, 0, 220, 255);
+}
+
+
+function drawObj(obj, r, g, b) {
+    for (let [index, item] of obj) {
+
+        const distanceObj = calculationBetweenTwoPoints(
+            gameObj.myPlayerObj.x, gameObj.myPlayerObj.y,
+            item.x, item.y,
+            gameObj.fieldWidth, gameObj.fieldHeight,
+            gameObj.raderCanvasWidth, gameObj.raderCanvasHeight
+        );
+
+        if (distanceObj.distanceX <= (gameObj.raderCanvasWidth / 2) && distanceObj.distanceY <= (gameObj.raderCanvasHeight / 2)) {
+
+            const degreeDiff = calcDegreeDiffFromRadar(gameObj.deg, distanceObj.degree);
+            const toumeido = calcOpacity(degreeDiff);
+
+            gameObj.ctxRader.fillStyle = `rgba(${r}, ${g}, ${b}, ${toumeido})`;
+            gameObj.ctxRader.beginPath();
+            gameObj.ctxRader.arc(distanceObj.drawX, distanceObj.drawY, gameObj.itemRadius, 0, Math.PI * 2, true);
+            gameObj.ctxRader.fill();
+        }
+    }
+}
+
+
+function calculationBetweenTwoPoints(pX, pY, oX, oY, gameWidth, gameHeight, raderCanvasWidth, raderCanvasHeight) {
+    let distanceX = 99999999;
+    let distanceY = 99999999;
+    let drawX = null;
+    let drawY = null;
+
+    if (pX <= oX) {
+        // 右から
+        distanceX = oX - pX;
+        drawX = (raderCanvasWidth / 2) + distanceX;
+        // 左から
+        let tmpDistance = pX + gameWidth - oX;
+        if (distanceX > tmpDistance) {
+            distanceX = tmpDistance;
+            drawX = (raderCanvasWidth / 2) - distanceX;
+        }
+
+    } else {
+        // 右から
+        distanceX = pX - oX;
+        drawX = (raderCanvasWidth / 2) - distanceX;
+        // 左から
+        let tmpDistance = oX + gameWidth - pX;
+        if (distanceX > tmpDistance) {
+            distanceX = tmpDistance;
+            drawX = (raderCanvasWidth / 2) + distanceX;
+        }
+    }
+
+    if (pY <= oY) {
+        // 下から
+        distanceY = oY - pY;
+        drawY = (raderCanvasHeight / 2) + distanceY;
+        // 上から
+        let tmpDistance = pY + gameHeight - oY;
+        if (distanceY > tmpDistance) {
+            distanceY = tmpDistance;
+            drawY = (raderCanvasHeight / 2) - distanceY;
+        }
+
+    } else {
+        // 上から
+        distanceY = pY - oY;
+        drawY = (raderCanvasHeight / 2) - distanceY;
+        // 下から
+        let tmpDistance = oY + gameHeight - pY;
+        if (distanceY > tmpDistance) {
+            distanceY = tmpDistance;
+            drawY = (raderCanvasHeight / 2) + distanceY;
+        }
+    }
+
+    const degree = calcTwoPointsDegree(drawX, drawY, raderCanvasWidth / 2, raderCanvasHeight / 2);
+
+    return {
+        distanceX,
+        distanceY,
+        drawX,
+        drawY,
+        degree
+    };
+}
+
+function calcTwoPointsDegree(x1, y1, x2, y2) {
+    const radian = Math.atan2(y2 - y1, x2 - x1);
+    const degree = radian * 180 / Math.PI + 180;
+    return degree;
+}
+
+function calcDegreeDiffFromRadar(degRader, degItem) {
+    let diff = degRader - degItem;
+    if (diff < 0) {
+        diff += 360;
+    }
+
+    return diff;
+}
+
+function calcOpacity(degreeDiff) {
+    const deleteDeg = 270;
+    degreeDiff = degreeDiff > deleteDeg ? deleteDeg : degreeDiff; // もう少しだけ暗くするコツ
+    return (1 - degreeDiff / deleteDeg).toFixed(2);
 }
